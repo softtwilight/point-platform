58fb5787169b7c1c6924d53c314a298dcabaf587
jinryou.you <jinryou.you@rakuten.com>
2023-01-23 22:07:37 +0900

    Init project

---
+++ src/main/kotlin/com/anymind/pointplatform/PointPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.anymind.pointplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointPlatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<PointPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/anymind/pointplatform/PointPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anymind.pointplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PointPlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
